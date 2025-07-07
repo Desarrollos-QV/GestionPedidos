3a472c6adc01529421278b1a232096b6b98328f3
Kiibo-Groups <kiibogroups@gmail.com>
2025-07-07 17:15:58 -0600

    constants bug

--- app/CentralLogics/helpers.php
+++ app/CentralLogics/helpers.php
@@ -25,12 +25,12 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
-if (!defined('CACHE_BUSINESS_SETTINGS_TABLE')) {
-    define('CACHE_BUSINESS_SETTINGS_TABLE', 'business_settings');
-}
-if (!defined('CACHE_LOGIN_SETUP_TABLE')) {
-    define('CACHE_LOGIN_SETUP_TABLE', 'login_setup');
-}
+// if (!defined('CACHE_BUSINESS_SETTINGS_TABLE')) {
+//     define('CACHE_BUSINESS_SETTINGS_TABLE', 'business_settings');
+// }
+// if (!defined('CACHE_LOGIN_SETUP_TABLE')) {
+//     define('CACHE_LOGIN_SETUP_TABLE', 'login_setup');
+// }
 
 class Helpers
 {
