882daa4967df052064b0455255ff05bcd47a9476
Kiibo-Groups <kiibogroups@gmail.com>
2025-07-07 16:35:50 -0600

    add domain in TrustHosts

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -15,6 +15,8 @@ class TrustHosts extends Middleware
     {
         return [
             $this->allSubdomainsOfApplicationUrl(),
+            'https://gestiondpedidos.web.app/',
+            'https://gestiondpedidos.firebaseapp.com/',
             'localhost'
         ];
     }
